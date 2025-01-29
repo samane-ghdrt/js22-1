ca170fa4a0e1bf296a311d5baac13a40107902fb
samane ghodrati <samane.ghodrati@gmail.com>
2025-01-28 22:58:35 -0900

    add

---
+++ exercise1(random-advice)/random-advoce.md
@@ -0,0 +1,7 @@
+Your challenge is to build out this advice generator app using the [Advice Slip API](https://api.adviceslip.com) and get it looking as close to the design as possible.
+
+the user of app should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
---
+++ exercise2(planet)/fetch_planetsss.js
@@ -0,0 +1,10 @@
+//1. The URL where our planet data is located is: "https://handlers.education.launchcode.org/static/planets.json".
+//2. Add the code to fetch this URL
+//   -The data you receive as a response should be an array containing 6 objects
+//3. show the first planet name and distance(first index of the array data)
+//4. let's dynamically change which planet's info we're displaying each time the element with id "destination" is clicked. To do this:
+//      a) Declare a counter variable, index that changes each time a click event takes place.
+//      b) Use the value of index as the position in the planets array to use in the template literal.
+//      c) Lastly, to ensure that the value of the index does not surpass the length of the planets array, implement a mechanism to control the maximum allowed value for the index
+
+////////Answer///////////
